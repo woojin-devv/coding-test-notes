c48071a1186a872996a32f67091b161b442cb090
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-07-30 15:48:29 +0900

    [Passed] 금 채굴하기 (53ms, 16MB)

--- codetree/trail4/금 채굴하기/gold-mining.py
+++ codetree/trail4/금 채굴하기/gold-mining.py
@@ -6,15 +6,12 @@ dys = [-1, 0, 1, 1, 1, 0, -1, -1]
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-
 def gold_mining(x, y, k):
     gold = 0
 
-    # 중심에서 상하좌우로 k만큼 떨어진 범위 확인
     for i in range(x - k, x + k + 1):
         for j in range(y - k, y + k + 1):
 
-            # 격자 안이고, 다이아몬드 범위 내부인 경우
             if (
                 in_range(i, j)
                 and abs(x - i) + abs(y - j) <= k
@@ -24,31 +21,6 @@ def gold_mining(x, y, k):
 
     if pow(k, 2) + pow(k + 1, 2) <= m * gold:
         return gold
-    else:
-        return 0
-
-    idx = 1
-    gold = 0
-
-    for dx, dy in zip(dxs, dys):
-        if idx % 2 != 0:
-            for i in range(k-1):
-                nx = x + dx
-                ny = y + dx
-
-                if in_range(nx, ny) and grid[nx][ny] == 1:
-                    gold += 1
-        else:
-            for i in range(k):            
-                nx = x + dx
-                ny = y + dy 
-
-                if in_range(nx, ny) and grid[nx][ny] == 1:
-                    gold += 1
-        idx += 1
-
-    if pow(k, 2) + pow(k+1, 2) <= m * gold :
-        return gold 
     else:
         return 0
 
