3367ae2cc1ccac910e2c9d2f9e6fa69cd1572e4d
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-08-17 11:18:24 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,15 @@
+SELECT 
+    '/home/grep/src/' || BOARD_ID 
+    || '/' || FILE_ID
+    || FILE_NAME 
+    || FILE_EXT AS FILE_PATH
+FROM USED_GOODS_FILE
+WHERE BOARD_ID IN (
+        SELECT BOARD_ID
+        FROM USED_GOODS_BOARD
+        ORDER BY VIEWS DESC
+        FETCH FIRST ROW ONLY
+        )
+ORDER BY FILE_ID DESC;
+
+
