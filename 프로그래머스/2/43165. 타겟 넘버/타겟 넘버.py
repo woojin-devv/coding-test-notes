ec2228b94d4484f6d7ac5ee3552bcddb8d6ec4f8
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-08-22 14:44:56 +0900

    [level 2] Title: 타겟 넘버, Time: 162.93 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,32 +1,17 @@
-
-# result 배열에 방문 처리 되어 가진 값을 append
-result = []
-
-def dfs(start, curr_sum, arr):
-    for i in range(start, len(arr)):
-        # 방문 처리된 값을 *2 만큼 뺌.
-        curr_sum -= (arr[i] * 2)
-        result.append(curr_sum)
-    
-        dfs(i+1, curr_sum, arr)
-        
-        curr_sum += (arr[i] * 2)
-        
-    return result
-
 def solution(numbers, target):
-    cnt = 0 
-    # 초기 합 
-    init_sum = sum(numbers)
+    answer = 0
     
-    # result 배열에서 target을 찾아 count += 1
-    result = dfs(0, init_sum, numbers)
+    def dfs(total, idx):
+        nonlocal answer
+        # 종료조건 
+        if idx == len(numbers):
+            if total == target:
+                answer += 1
+            return 
+        dfs(total + numbers[idx], idx+1)
+
+        dfs(total - numbers[idx], idx+1)
+
+    dfs(0, 0)
     
-    # 모두 양수인 경우 
-    result.append(init_sum)
-    
-    for el in result:
-        if el == target:
-            cnt += 1 
-            
-    return cnt
+    return answer
