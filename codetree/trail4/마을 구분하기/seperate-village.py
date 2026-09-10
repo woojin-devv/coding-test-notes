df5c5ed4ed1dd86bc4470d397cdc8a80649df46c
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-09-10 14:15:27 +0900

    [Passed] 마을 구분하기 (51ms, 16MB)

--- codetree/trail4/마을 구분하기/seperate-village.py
+++ codetree/trail4/마을 구분하기/seperate-village.py
@@ -1,39 +1,43 @@
-import sys
-sys.setrecursionlimit(10 ** 4)
-
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
+
 visited = [[False] * n for _ in range(n)]
-answer = []
 
-# 북 -> 북동 -> 동 -> 동남 -> 남 -> 남서 -> 서 -> 북서
-# dxs = [-1, -1, 0, 1, 1, 1, 0, -1]
-# dys = [0, 1, 1, 1, 0, -1, 0, -1]
-
-# 상, 하, 좌, 우
 dxs = [-1, 1, 0, 0]
 dys = [0, 0, -1, 1]
+
+answers = []
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
+def can_go(x, y):
+    return (in_range(x, y) 
+        and not visited[x][y]
+        and grid[x][y] == 1)
 
-def dfs(i, j):
-    cnt = 1
-    for nx, ny in zip(dxs, dys):
-        if in_range(i + nx, j + ny) and not visited[i + nx][j + ny] and grid[i + nx][j + ny]:
-            visited[i + nx][j + ny] = True
-            cnt += dfs(i + nx, j + ny)
-    return cnt 
+def dfs(x, y):
+    count = 1 
+
+    for dx, dy in zip(dxs, dys):
+        nx = x + dx
+        ny = y + dy 
+
+        if can_go(nx, ny):
+            visited[nx][ny] = True
+            count += dfs(nx, ny)
+
+    return count
 
 for i in range(n):
     for j in range(n):
-        if not visited[i][j] and grid[i][j]:
+        if not visited[i][j] and grid[i][j] == 1:
             visited[i][j] = True
-            answer.append(dfs(i, j))
+            temp = dfs(i, j)
+            answers.append(temp)
 
-# 출력
-answer.sort()
-print(len(answer))
-for el in answer:
-    print(el)
+answers.sort()
+
+print(len(answers))
+for answer in answers:
+    print(answer)
