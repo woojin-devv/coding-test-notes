f3eb4aa615ea3de7ba2f2f2514fe6784411230c2
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-09-10 13:56:59 +0900

    [Passed] 두 방향 탈출 가능 여부 판별하기 (48ms, 16MB)

--- codetree/trail4/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ codetree/trail4/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,53 +1,39 @@
+import sys
+
+sys.setrecursionlimit(10000)
+
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
+dxs = [1, 0]
+dys = [0, 1]
 visited = [[False] * m for _ in range(n)]
+x, y = 0, 0
 
-# 동 남 
-dxs = [0, 1]
-dys = [1, 0]
+def in_range(nx, ny):
+    return 0 <= nx < n and 0 <= ny < m
 
-# 조건 (순서)
-    # 1. 오른쪽 진입 
-    # 2. 아래 진입 
-
-# 범위 안에 있는지 체크하는 함수 
-def in_range(x, y):
-    if 0 <= x < n and 0 <= y < m:
+def can_go(x, y):
+    if in_range(x, y) and grid[x][y] == 1 and not visited[x][y]:
         return True
     return False
 
-# can_go 함수
-    # 1. 격자 안에 있는 경우 
-    # 2. 진입하려고 하는 방향에 뱀이 존재하지 않을 경우 
-    # 3. 방문하지 않은 경우
-def can_go(nx, ny):
-    if not in_range(nx, ny):
-        return False
-    if grid[nx][ny] == 0:
-        return False
-    if visited[nx][ny]:
-        return False
-    return True
-
-# dfs 
 def dfs(x, y):
     if x == n - 1 and y == m - 1:
         return 1
 
-    for off_x, off_y in zip(dxs, dys):
-        nx = x + off_x
-        ny = y + off_y
+    for dx, dy in zip(dxs, dys):
+        nx = x + dx
+        ny = y + dy
 
         if can_go(nx, ny):
             visited[nx][ny] = True
             
             if dfs(nx, ny):
-                return True
-    return False
+                return 1
     
-visited[0][0] = True
+    return 0
 
-if dfs(0, 0):
-    print(1)
-else:
-    print(0)
+
+visited[0][0] = True
+print(dfs(0, 0))
+    
