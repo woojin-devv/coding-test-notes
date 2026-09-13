eaebd698d3b4b0af07c391c620b676c6aad10d87
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-09-13 17:09:59 +0900

    [level 2] Title: 가격이 제일 비싼 식품의 정보 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -0,0 +1,9 @@
+SELECT PRODUCT_ID, 
+       PRODUCT_NAME, 
+       PRODUCT_CD, 
+       CATEGORY, 
+       PRICE
+FROM FOOD_PRODUCT
+WHERE PRICE IN (SELECT MAX(PRICE)
+                FROM FOOD_PRODUCT
+                )
