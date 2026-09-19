4b08c51cc16e5346af7de3c6511dac6fe7cbd06b
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-09-19 20:10:57 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,13 +1,2 @@
 def solution(s1, s2):
-    answer = 0
-    
-    if len(s1) > len(s2):
-        for el in s2:
-            if el in s1:
-                answer += 1
-    else:
-        for el in s1:
-            if el in s2:
-                answer += 1
-                
-    return answer
+    return len(set(s1) & set(s2))
