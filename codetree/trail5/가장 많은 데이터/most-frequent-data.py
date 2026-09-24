2bee235196a3e5c85dc9eaeac3f89b58515c7b67
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-09-24 10:54:04 +0900

    [Passed] 가장 많은 데이터 (205ms, 33MB)

--- codetree/trail5/가장 많은 데이터/most-frequent-data.py
+++ codetree/trail5/가장 많은 데이터/most-frequent-data.py
@@ -1,9 +1,10 @@
 from collections import Counter as c
-
 n = int(input())
 words = [input() for _ in range(n)]
 
 freq = c(words)
-most_b = freq.most_common(1)
 
-print(most_b[0][1])
+# help(sorted)
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+print(freq[0][1])
