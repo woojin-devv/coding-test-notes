a31f1847d96d9154ec071797fb3cc2cf7a4ff7b8
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-09-24 10:27:44 +0900

    [Passed] hashmap 기본 (236ms, 24MB)

--- codetree/trail5/hashmap 기본/hashmap-basic.py
+++ codetree/trail5/hashmap 기본/hashmap-basic.py
@@ -1,25 +1,20 @@
 n = int(input())
 commands = []
+d = dict()
+
 for _ in range(n):
     line = input().split()
     cmd = line[0]
     k = int(line[1])
     if cmd == "add":
         v = int(line[2])
-        commands.append((cmd, k, v))
-    else:
-        commands.append((cmd, k))
-
-d = dict()
-
-for el in commands:
-    if el[0] == "add":
-        d[el[1]] = el[2]
-    elif el[0] == "find":
-        if el[1] in d:
-            print(d[el[1]])
-        else:
+        d[k] = v
+    elif cmd == "find":
+        if k not in d:
             print("None")
-    elif el[0] == "remove":
-        d.pop(el[1])
+        else:
+            print(d[k])
+    elif cmd == "remove":
+        d.pop(k)
+
 
